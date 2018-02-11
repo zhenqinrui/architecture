236b31e13018a801428ce16d0e4494b9be2934cb
zengqinrui <zengqinrui@szy.cn>
2018-02-11 16:02:49 +0800

    工厂方法模式重命名

---
+++ architect_absfactory/src/main/java/architect/rui/com/architect_absfactory/MainActivity.java
@@ -0,0 +1,13 @@
+package architect.rui.com.architect_absfactory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
